7d48ee2738f2ee734ee4ce4fcd9c1bd16d96c274
Lim Jie <limjie1994@gmail.com>
2019-02-26 06:37:18 +0800

    Added Simple design to select user and tabs to display data

--- client/src/Main/Main.tsx
+++ client/src/Main/Main.tsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Nav, Dropdown, InputGroup, FormControl, Col, Row } from "react-bootstrap";
+import DropdownItem from 'react-bootstrap/DropdownItem';
+import DropdownMenu from 'react-bootstrap/DropdownMenu';
 
 class Main extends Component {
   state = {
     data: { title: '', description: '' },
+    user: "Oscar",
+    tab: "Items",
   }
 
   async componentDidMount() {
@@ -15,18 +20,62 @@ class Main extends Component {
     })
   }
 
+  changeUser = (user: string) => {
+    this.setState({user})
+  } 
+
+  changeTab = (tab: string) => {
+    this.setState({tab})
+  }
+
   render() {
     const { data } = this.state
     if (data === null) return <p>Loading ...</p>
 
     return (
+      // <div className="container">
+      //   <div className="row">
+      //     <div className="jumbotron col-12 text-center">
+      //       <h1 className="display-3">{data.title}</h1>
+      //       <p className="lead">{data.description}</p>
+      //     </div>
+      //   </div>
+      // </div>
       <div className="container">
-        <div className="row">
-          <div className="jumbotron col-12 text-center">
-            <h1 className="display-3">{data.title}</h1>
-            <p className="lead">{data.description}</p>
-          </div>
-        </div>
+        <Row>
+          <Col md="auto">
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              Select User
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item onSelect={() => this.changeUser("Oscar")}>Oscar</Dropdown.Item>
+                <Dropdown.Item onSelect={() => this.changeUser("Nicholas")}>Nicholas</Dropdown.Item>
+                <Dropdown.Item onSelect={() => this.changeUser("Lim Jie")}>Lim Jie</Dropdown.Item>
+                <Dropdown.Item onSelect={() => this.changeUser("Julian")}>Julian</Dropdown.Item>
+              </Dropdown.Menu>
+          </Dropdown>
+          </Col>
+          <Col>
+          <h4 className="user">
+  	        {this.state.user}
+          </h4>
+          </Col>
+        </Row>
+        <Nav variant="tabs" activeKey={this.state.tab}>
+          <Nav.Item>
+            <Nav.Link eventKey="Items" onSelect={() => this.changeTab("Items")}>Items</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="Loans" onSelect={() => this.changeTab("Loans")}>Loans</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="Other Users" onSelect={() => this.changeTab("Other Users")}>Other Users</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="Past Loans" onSelect={() => this.changeTab("Past Loans")}>Past Loans</Nav.Link>
+          </Nav.Item>
+        </Nav>
       </div>
     )
   }
